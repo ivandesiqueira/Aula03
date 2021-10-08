b3c36442d97ebfaf88fdfd5227012624c4a1824a
Ivan de Siqueira <ivandesiqueira@gmail.com>
2021-10-08 20:10:04 -0300

    Implementado o operador &&

--- src/OperadorE.java
+++ src/OperadorE.java
@@ -3,18 +3,25 @@ import java.util.Scanner;
 public class OperadorE {
 
 	public static void main(String[] args) {
-		/*Faça um programa que verifique se o usuário digitado é igual a 'administrador' e se a senha digitada é igual a '123'*/
+		/*
+		 * Faça um programa que verifique se o usuário digitado é igual a
+		 * 'administrador' e se a senha digitada é igual a '123'
+		 */
 		Scanner leitor = new Scanner(System.in);
 		String usuario, senha;
 		System.out.println("SISTEMA DE LOGIN");
 		System.out.println("Por favor, digite seu username:");
 		usuario = leitor.next();
 		System.out.println("Por favor, digite sua senha:");
-		senha = leitor.next();	
-		//Aqui é preciso criar um if que verifique as duas condições para o login ser bem sucedido
-		
-		
-		
+		senha = leitor.next();
+		// Aqui é preciso criar um if que verifique as duas condições para o login ser
+		// bem sucedido
+		if (usuario.equals("Administrador") && senha.equals("123")) {
+			System.out.println("Login autorizado!");
+		} else {
+			System.out.println("Usuário ou senha incorreto!");
+		}
+
 		leitor.close();
 
 	}
